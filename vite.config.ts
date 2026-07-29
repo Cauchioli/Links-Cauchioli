ceae5e27f526828f922dae87dfa2a525b7d8c1e5
Cauchioli <51243395+Cauchioli@users.noreply.github.com>
2026-07-29 11:29:54 -0300

    Feat: Adicionado vertex.html em rollupOptions do vite.config.ts para geracao do dist/vertex.html no Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        vertex: path.resolve(__dirname, "vertex.html"),
+      },
+    },
+  },
 }));
